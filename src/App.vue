1e675f922225182f634348b9d60bc19488aca4c1
LADY <111048674+mialyramihone@users.noreply.github.com>
2025-05-08 23:07:00 +0300

    add

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script setup>
+import { ref, onMounted } from 'vue';
+import { useRoute } from 'vue-router';
+import { useStore } from 'vuex';
+import { useI18n } from 'vue-i18n';
+import { useHead } from '@vueuse/head';
+import { useMeta } from 'vue-meta';
+</script>
+
+<template>
+
+
+</template>
+
+<style scoped>
+/* Tous les styles du loader */
+</style>
