cc4318e9202de6357512c4f3b6490e3dd94962ed
Raven <nathaliorakotobe3@gmail.com>
2025-05-12 15:53:46 +0300

    Description de tes changements

--- src/main.js
+++ src/main.js
@@ -1,5 +1,10 @@
 import { createApp } from 'vue'
-import './style.css'
 import App from './App.vue'
+
+// Importez la police (via Google Fonts)
+const link = document.createElement('link')
+link.href = 'https://fonts.googleapis.com/css2?family=Tektur:wght@400;700&display=swap'
+link.rel = 'stylesheet'
+document.head.appendChild(link)
 
 createApp(App).mount('#app')
